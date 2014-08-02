64040026b314ecd28cceee4bec0cf7bcee70c15e
aga-mn <molda.nieweglowska@gmail.com>
2014-08-02 12:48:15 +0200

    CRUD operations

--- src/practice/dto/UserDetails.java
+++ src/practice/dto/UserDetails.java
@@ -1,34 +1,18 @@
 package practice.dto;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
-@Table(name="USER_DETAILS")
+
 public class UserDetails {
 	
 	@Id
 	@GeneratedValue	(strategy=GenerationType.AUTO)		
 	private int userId;
 	private String userName;
-	
-	@OneToMany(cascade=CascadeType.PERSIST)
-	private Collection<Vehicle> vehicles=new ArrayList<Vehicle>();
-	
-	public Collection<Vehicle> getVehicles() {
-		return vehicles;
-	}
-	public void setVehicles(Collection<Vehicle> vehicles) {
-		this.vehicles = vehicles;
-	}
 	
 	public int getUserId() {
 		return userId;
--- src/practice/hibernate/HibernateTest.java
+++ src/practice/hibernate/HibernateTest.java
@@ -4,36 +4,36 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import practice.dto.FourWheeler;
-import practice.dto.TwoWheeler;
-import practice.dto.Vehicle;
+import practice.dto.UserDetails;
 
 public class HibernateTest {
 
 	public static void main(String[] args) {
 	
-		Vehicle vehicle=new Vehicle();
-		vehicle.setVehicleName("Vehicle");
-		
-		TwoWheeler bike=new TwoWheeler();
-		bike.setVehicleName("Bike");
-		bike.setSteeringHandle("Bike steering handle");
-		
-		FourWheeler car = new FourWheeler();
-		car.setVehicleName("Four Wheeler");
-		car.setSteeringWheel("Car steering wheel");
-		
+				
 		@SuppressWarnings("deprecation")
 		SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
 		Session session=sessionFactory.openSession();
 		
 		session.beginTransaction();
-		session.persist(vehicle);
-		session.persist(car);
-		session.persist(bike);
+		
+		//CREATE
+		/*for(int i=0;i<10;i++){
+			UserDetails user=new UserDetails();
+			user.setUserName("User "+i);
+			session.save(user);
+		}*/
+		
+		//READ
+		UserDetails user=(UserDetails)session.get(UserDetails.class, 5);
+		
+		//UPDATE
+		user.setUserName("Updated UserName");
+		session.update(user);
+		//DELETE
+		//session.delete(user);
 		session.getTransaction().commit();
 		session.close();
-		
 		
 
 	}

e026b99d28f4ca2edfb64d280d5493fdd5658c5e
aga-mn <molda.nieweglowska@gmail.com>
2014-08-02 11:37:38 +0200

    Inheritance. Table-per-class strategy

--- src/practice/dto/TwoWheeler.java
+++ src/practice/dto/TwoWheeler.java
@@ -1,10 +1,9 @@
 package practice.dto;
 
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("Bike")
+
 public class TwoWheeler extends Vehicle {
 	
 	private String steeringHandle;
--- src/practice/dto/Vehicle.java
+++ src/practice/dto/Vehicle.java
@@ -1,7 +1,5 @@
 package practice.dto;
 
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,11 +7,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(
-		name="VEHICLE_TYPE",
-		discriminatorType=DiscriminatorType.STRING
-		)
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 
 public class Vehicle {
 
